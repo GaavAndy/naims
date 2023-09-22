6ad0cfb8ac2f45dfeeb5de4a84fed440d310deb3
Asaaga <simonasarga@gmail.com>
2023-09-22 06:31:30 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "color-primary": "#003600",
-        "color-primary-light": "#037003",
+        "color-primary": "green-700",
+        "color-primary-light": "#0a682d",
         "color-primary-dark": "#002509",
-        "color-secondary": "#010131",
+        "color-secondary": "#024b33",
         "color-gray": "#333",
         "color-white": "#fff",
         "color-blob": "#AA27DF",
-      }
+      },
+      fontFamily: {
+        'rubik-gemstones': ['"Rubik Gemstones"', 'sans'],
+      },
     },
     container: {
       center: true,
